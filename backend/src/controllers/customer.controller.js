bc8b6ffa520b8105c67d5f7eab5b62ee1270eb1f
lamtell <jhormnjair21@gmail.com>
2022-04-17 15:44:09 -0500

    Arreglando los middleware de las formulas

--- backend/src/controllers/customer.controller.js
+++ backend/src/controllers/customer.controller.js
@@ -1,164 +1,302 @@
 const Customer = require('../models/customer.model.js');
-const Psychobiological = require("../models/psycho.model.js");
+const Psychobiological = require('../models/psycho.model.js');
 const Lab = require('../models/lab.model.js');
+const formulas = require('../middleware/customer.middleware.js');
 const express = require('express');
 const { default: mongoose } = require('mongoose');
 
 exports.addCustomer = async (req, res) => {
-    try {
-        const customer = new Customer(req.body);
-        await customer.save();
+	try {
+		const customer = new Customer(req.body);
+		await customer.save();
 
-        res.status(201).json({ ok: true, message: 'Cliente agregado con éxito' });
-    } catch (error) {
-        if (error.code === 11000) return res.status(400).json({ ok: false, message: 'Verifique la información del usuario.' });
+		res.status(201).json({ ok: true, message: 'Cliente agregado con éxito' });
+	} catch (error) {
+		if (error.code === 11000)
+			return res
+				.status(400)
+				.json({ ok: false, message: 'Verifique la información del usuario.' });
 
-        res.status(400).json({ ok: false, message: 'No se pudo ingresar al cliente.' });
-    }
-}
+		res
+			.status(400)
+			.json({ ok: false, message: 'No se pudo ingresar al cliente.' });
+	}
+};
 exports.listID = async (req, res, _id) => {
-    try {
-        const customer = await Customer.findById({ _id })
-    } catch (error) {
-
-    }
-}
+	try {
+		const customer = await Customer.findById({ _id });
+	} catch (error) {}
+};
 exports.addPsychologicalHabit = async (req, res) => {
+	const { _id, psychologicalHabit, allergies, supplements, intolerances } =
+		req.body;
+	let error;
 
-    const { _id, psychologicalHabit, allergies, supplements, intolerances } = req.body;
-    let error;
+	psychologicalHabit.forEach(async (habit) => {
+		const psycho = await Psychobiological.findById({ _id: habit.idHabit });
+		if (psycho) {
+			Customer.updateOne(
+				{
+					_id,
+				},
+				{
+					$push: {
+						psychologicalHabit: {
+							idHabit: habit.idHabit,
+							typeTimes: habit.typeTimes,
+							times: habit.times,
+							descriptionHabit: habit.descriptionHabit,
+						},
+					},
+				},
+				(err) => {
+					if (err) {
+						error = err;
+					}
+				}
+			);
+		}
+		Customer.updateOne({ _id }, { $set: { allergies: allergies } }, (err) => {
+			if (error) error = err;
+		});
+		Customer.updateOne(
+			{ _id },
+			{ $set: { supplements: supplements } },
+			(err) => {
+				if (error) error = err;
+			}
+		);
+		Customer.updateOne(
+			{ _id },
+			{ $set: { intolerances: intolerances } },
+			(err) => {
+				if (error) error = err;
+			}
+		);
+	});
 
-    psychologicalHabit.forEach(async (habit) => {  
-        const psycho = await Psychobiological.findById({_id: habit.idHabit});
-        if(psycho){
-            Customer.updateOne(
-                {
-                    _id
-                }, {
-                $push: {
-                    "psychologicalHabit": {
-                        idHabit: habit.idHabit,
-                        typeTimes: habit.typeTimes,
-                        times: habit.times,
-                        descriptionHabit: habit.descriptionHabit
-                    }
-                }
-            }, (err) => {
-                if (err) {
-                    error = err;
-                }
-            }
-            );
-        }
-        Customer.updateOne({_id}, { $set: { 'allergies': allergies}}, (err)=>{ if(error) error = err})
-        Customer.updateOne({_id}, { $set: { 'supplements': supplements}}, (err)=>{ if(error) error = err})
-        Customer.updateOne({_id}, { $set: { 'intolerances': intolerances}}, (err)=>{ if(error) error = err})
-    });
-    
-    if (error) {
-        return res.status(400).json({ ok: false, message: 'No se pudo ingresar los hábitos alimenticios.' });
-    } else {
-        res.status(201).json({ ok: true, message: "Se agregó correctamente las hábitos alimenticios." })
-    }
-}
+	if (error) {
+		return res.status(400).json({
+			ok: false,
+			message: 'No se pudo ingresar los hábitos alimenticios.',
+		});
+	} else {
+		res.status(201).json({
+			ok: true,
+			message: 'Se agregó correctamente las hábitos alimenticios.',
+		});
+	}
+};
 
 exports.addFeedingHabits = async (req, res) => {
-    const { _id, feedingHabits } = req.body;
-    let error;
-    
-    feedingHabits.forEach(habit => {
-        Customer.updateOne({
-            _id
-        }, {
-            $push: {
-                'feedingHabits': {
-                    idHabit: habit.idHabit,
-                    name: habit.name,
-                    typeTimes: habit.typeTimes,
-                    times: habit.times,
-                    descriptionHabit: habit.descriptionHabit
-                }
-            }
-        }, (err) => {
-            if (err) error = err;
-        })
-    });
-    if (error) {
-        return res.status(400).json({ ok: false, message: 'No se pudo ingresar los hábitos alimenticios.' });
-    } else {
-        res.status(201).json({ ok: true, message: "Se agregó correctamente las hábitos alimenticios." });
-    }
-}
+	const { _id, feedingHabits } = req.body;
+	let error;
+
+	feedingHabits.forEach((habit) => {
+		Customer.updateOne(
+			{
+				_id,
+			},
+			{
+				$push: {
+					feedingHabits: {
+						idHabit: habit.idHabit,
+						name: habit.name,
+						typeTimes: habit.typeTimes,
+						times: habit.times,
+						descriptionHabit: habit.descriptionHabit,
+					},
+				},
+			},
+			(err) => {
+				if (err) error = err;
+			}
+		);
+	});
+	if (error) {
+		return res.status(400).json({
+			ok: false,
+			message: 'No se pudo ingresar los hábitos alimenticios.',
+		});
+	} else {
+		res.status(201).json({
+			ok: true,
+			message: 'Se agregó correctamente las hábitos alimenticios.',
+		});
+	}
+};
 exports.addLab = async (req, res) => {
-    const { _id, labs } = req.body;
-    let error;
-    
-    labs.forEach(lab => {
-        Customer.updateOne({
-            _id
-        }, {
-            $push: {
-                'labs': {
-                    idLab: lab.idLab,
-                    name: lab.name,
-                    value: lab.value,
-                    unit: lab.unit
-                }
-            }
-        }, (err) => {
-            if (err) error = err;
-        })
-    });
-    if (error) {
-        return res.status(400).json({ ok: false, message: 'No se pudo ingresar los hábitos alimenticios.' });
-    } else {
-        res.status(201).json({ ok: true, message: "Se agregó correctamente las hábitos alimenticios." });
-    }
-}
+	const { _id, labs } = req.body;
+	let error;
+
+	labs.forEach((lab) => {
+		Customer.updateOne(
+			{
+				_id,
+			},
+			{
+				$push: {
+					labs: {
+						idLab: lab.idLab,
+						name: lab.name,
+						value: lab.value,
+						unit: lab.unit,
+					},
+				},
+			},
+			(err) => {
+				if (err) error = err;
+			}
+		);
+	});
+	if (error) {
+		return res.status(400).json({
+			ok: false,
+			message: 'No se pudo ingresar los hábitos alimenticios.',
+		});
+	} else {
+		res.status(201).json({
+			ok: true,
+			message: 'Se agregó correctamente las hábitos alimenticios.',
+		});
+	}
+};
 exports.listCustomer = async (req, res) => {
-    try {
-        const customer = await Customer.find({});
-        res.status(200).json({ ok: true, message: "Se hizo la petición la manera correcta.", customer });
-        
-    } catch (error) {
-        res.status(400).json({ ok: false, error });
-    }
-}
+	try {
+		const customer = await Customer.find({});
+		res.status(200).json({
+			ok: true,
+			message: 'Se hizo la petición la manera correcta.',
+			customer,
+		});
+	} catch (error) {
+		res.status(400).json({ ok: false, error });
+	}
+};
 
 exports.idCustomer = async (req, res) => {
-    try {
-        const customer = await Customer.findById({ _id: req.params.id});
-        res.status(200).json({ ok: true, message: "El cliente fue encontra exitosamente.", customer });
-    } catch (error) {
-        res.status(400).json({ ok: false, message: "No se pudo encontrar al cliente."});
-    }
-}
+	try {
+		const customer = await Customer.findById({ _id: req.params.id });
+		res.status(200).json({
+			ok: true,
+			message: 'El cliente fue encontra exitosamente.',
+			customer,
+		});
+	} catch (error) {
+		res
+			.status(400)
+			.json({ ok: false, message: 'No se pudo encontrar al cliente.' });
+	}
+};
 
-exports.background = async (req, res) =>{
-    const {_id, personalHistory, familyBackground} = req.body;
-    let error;
+exports.background = async (req, res) => {
+	const { _id, personalHistory, familyBackground } = req.body;
+	let error;
 
-    Customer.updateOne({_id}, { $set: { 'personalHistory': personalHistory}}, (err)=>{ if(error) error = err})
-    Customer.updateOne({_id}, { $set: { 'familyBackground': familyBackground}}, (err)=>{ if(error) error = err})
-    
-    if(error){
-        res.status(400).json({ok: true, message: "No se pueden agregar los datos."});
-    }else{
+	Customer.updateOne(
+		{ _id },
+		{ $set: { personalHistory: personalHistory } },
+		(err) => {
+			if (error) error = err;
+		}
+	);
+	Customer.updateOne(
+		{ _id },
+		{ $set: { familyBackground: familyBackground } },
+		(err) => {
+			if (error) error = err;
+		}
+	);
 
-        res.status(200).json({ok: true, message: "Los datos fueron agregado con éxito."});
-    }
-}
+	if (error) {
+		res
+			.status(400)
+			.json({ ok: true, message: 'No se pueden agregar los datos.' });
+	} else {
+		res
+			.status(200)
+			.json({ ok: true, message: 'Los datos fueron agregado con éxito.' });
+	}
+};
 
-exports.listCustomerNutritionist = async (req, res)=>{
-    try {
-        const customer = await Customer.find({nutritionistId: req.body.nutritionistId});
+exports.listCustomerNutritionist = async (req, res) => {
+	try {
+		const customer = await Customer.find({
+			nutritionistId: req.body.nutritionistId,
+		});
 
-        res.status(200).json({ok: true, message: "Se encontró al cliente con éxito.", customer})
-    } catch (error) {
-        res.status(400).json({ok: true, message: "No se encontró al cliente"});
-    }
-}
+		res.status(200).json({
+			ok: true,
+			message: 'Se encontró al cliente con éxito.',
+			customer,
+		});
+	} catch (error) {
+		res.status(400).json({ ok: true, message: 'No se encontró al cliente' });
+	}
+};
 
-exports.formulas = async (req, res)=>{
-    res.status(200).json({ok: true, message: "Se agregó correctamente"})
-}
+exports.formulas = async (req, res) => {
+	let error;
+	const {
+		_id,
+		tallaCM,
+		circunferenciaDeMuniecaCM,
+		pesoActualKG,
+		pesoACalcular,
+	} = req.body;
+
+	Customer.updateOne(
+		{ _id },
+		{
+			$set: {
+				tallaCentrimetros: tallaCM,
+				circunferenciaDeMuniecaCentrimetros: circunferenciaDeMuniecaCM,
+			},
+		},
+		(err) => {
+			if (err)
+				return res
+					.status(400)
+					.json({ ok: false, message: 'No se puede guardas los datos.' });
+		}
+	);
+	if (pesoACalcular === 'Hamwi/Brocca') {
+		//console.log(typeof tallaCM)
+		if (tallaCM >= 152) {
+			exito = await formulas.pesoIdealHamwi(
+				_id,
+				tallaCM,
+				circunferenciaDeMuniecaCM
+			);
+			return res.status(200).json({
+				ok: true,
+				message: 'Se agrego correctamente peso ideal Hamwi.',
+			});
+		} else if (tallaCM < 152 && tallaCM >= 0) {
+			console.log('Entro');
+			exito = await formulas.pesoIdealBrocca(
+				_id,
+				tallaCM,
+				circunferenciaDeMuniecaCM
+			);
+			return res.status(200).json({
+				ok: true,
+				message: 'Se agrego correctamente peso ideal Brocca.',
+			});
+		} else {
+			return res
+				.status(400)
+				.json({ ok: false, message: 'Ingrese los correctamente.' });
+		}
+	} else if (pesoACalcular === 'Clínica Mayo; West') {
+		console.log('entro');
+		exito = await formulas.pesoIdealClinicaMayoWest(_id, tallaCM);
+		return res.status(200).json({
+			ok: false,
+			message: 'Se agrego correctamente peso ideal Clínica Mayo; West.',
+		});
+	} else if (pesoACalcular === 'IMC') {
+	} else if (pesoACalcular === 'Peso Ajustado') {
+	}
+	next();
+};
--- backend/src/middleware/customer.middleware.js
+++ backend/src/middleware/customer.middleware.js
@@ -1,204 +1,167 @@
-const Customer = require("../models/customer.model.js");
-const elegirPesoIdeal = async (req, res, next) => {
-  let error;
-  const {
-    _id,
-    tallaCM,
-    circunferenciaDeMuniecaCM,
-    pesoActualKG,
-    pesoACalcular,
-  } = req.body;
+const Customer = require('../models/customer.model.js');
 
-  Customer.updateOne(
-    { _id },
-    {
-      $set: {
-        tallaCentrimetros: tallaCM,
-        circunferenciaDeMuniecaCentrimetros: circunferenciaDeMuniecaCM,
-      },
-    },
-    (err) => {
-      if (err) error = err;
-    }
-  );
-  if (error) next("Error al actualizar datos");
-  else {
-    if (pesoACalcular === "Hamwi/Brocca") {
-      //console.log(typeof tallaCM)
-      if (tallaCM >= 152) {
-        exito = await pesoIdealHamwi(_id, tallaCM, circunferenciaDeMuniecaCM);
-      } else if (tallaCM < 152 && tallaCM >= 0) {
-        console.log("Entro");
-        exito = await pesoIdealBrocca(_id, tallaCM, circunferenciaDeMuniecaCM);
-        next()
-      } else {
-        next("Verifique los datos")
-      }
-    } else if (pesoACalcular === "Clínica Mayo; West") {
-      console.log("entro")
-      exito = await pesoIdealClinicaMayoWest(_id, tallaCM);
-      next()
-    } else if (pesoACalcular === "IMC") {
-    } else if (pesoACalcular === "Peso Ajustado") {
-    }
-    next();
-  }
-};
 const pesoIdealHamwi = async (_id, tallaCM, circunferenciaDeMuniecaCM) => {
-  let error, pesoIdealHamwi, contexturaGrantValor, contexturaGrantTipo;
-  try {
-    if (typeof tallaCM === "number") {
-      //console.log(idCustomer);
-      const customer = await Customer.findById({ _id });
-      console.log(customer);
+	let error, pesoIdealHamwi, contexturaGrantValor, contexturaGrantTipo;
+	try {
+		if (typeof tallaCM === 'number') {
+			//console.log(idCustomer);
+			const customer = await Customer.findById({ _id });
+			//console.log(customer);
 
-      if (customer.sex === "M") {
-        pesoIdealHamwi = (tallaCM - 152) * 1.08 + 48.5;
+			if (customer.sex === 'M') {
+				pesoIdealHamwi = (tallaCM - 152) * 1.08 + 48.5;
 
-        contexturaGrantValor = tallaCM / circunferenciaDeMuniecaCM;
+				contexturaGrantValor = tallaCM / circunferenciaDeMuniecaCM;
 
-        if (contexturaGrantValor > 10.4) {
-          contexturaGrantTipo = "Contextura Pequeña";
-        } else if (contexturaGrantValor > 9.6 && contexturaGrantValor <= 10.4) {
-          contexturaGrantTipo = "Contextura Mediana";
-          req.locals = { contexturaMediana };
-        } else if (contexturaGrantValor <= 9.6) {
-          contexturaGrantTipo = "Contextura Grande";
-        }
-        // PESO IDEAL O TEÓRICO: 2. Bocca
-      } else {
-        pesoIdealHamwi = (tallaCM - 152) * 0.88 + 48.5;
+				if (contexturaGrantValor > 10.4) {
+					contexturaGrantTipo = 'Contextura Pequeña';
+				} else if (contexturaGrantValor > 9.6 && contexturaGrantValor <= 10.4) {
+					contexturaGrantTipo = 'Contextura Mediana';
+					req.locals = { contexturaMediana };
+				} else if (contexturaGrantValor <= 9.6) {
+					contexturaGrantTipo = 'Contextura Grande';
+				}
+				// PESO IDEAL O TEÓRICO: 2. Bocca
+			} else {
+				pesoIdealHamwi = (tallaCM - 152) * 0.88 + 48.5;
 
-        contexturaGrantValor = tallaCM / circunferenciaMunieca;
+				contexturaGrantValor = tallaCM / circunferenciaMunieca;
 
-        if (contexturaGrantValor > 11) {
-          contexturaGrantTipo = "Contextura Pequeña";
-        } else if (contexturaGrantValor > 10.4 && contexturaGrantValor <= 11) {
-          contexturaGrantTipo = "Contextura Mediana";
-        } else if (contexturaGrantValor <= 10.11) {
-          contexturaGrantTipo = "Contextura Grande";
-        }
-      }
+				if (contexturaGrantValor > 11) {
+					contexturaGrantTipo = 'Contextura Pequeña';
+				} else if (contexturaGrantValor > 10.4 && contexturaGrantValor <= 11) {
+					contexturaGrantTipo = 'Contextura Mediana';
+				} else if (contexturaGrantValor <= 10.11) {
+					contexturaGrantTipo = 'Contextura Grande';
+				}
+			}
 
-      //Actualizando los datos del cliente
-      Customer.updateOne(
-        { _id },
-        {
-          $set: {
-            pesoIdealHamwi: pesoIdealHamwi,
-            contexturaGrantValor: contexturaGrantValor,
-            contexturaGrantTipo: contexturaGrantTipo,
-          },
-        },
-        (err) => {
-          if (err) error = err;
-        }
-      );
-      return true;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    return false;
-  }
+			//Actualizando los datos del cliente
+			Customer.updateOne(
+				{ _id },
+				{
+					$set: {
+						pesoIdealHamwi: pesoIdealHamwi,
+						contexturaGrantValor: contexturaGrantValor,
+						contexturaGrantTipo: contexturaGrantTipo,
+					},
+				},
+				(err) => {
+					if (err) error = err;
+				}
+			);
+			return true;
+		} else {
+			return false;
+		}
+	} catch (error) {
+		return false;
+	}
 };
 const pesoIdealBrocca = async (_id, tallaCM, circunferenciaMunieca) => {
-  let error = null, contexturaGrantTipo, contexturaGrantValor, pesoIdealBrocca;
-  const customer = await Customer.findById({ _id });
-  console.log(customer, "sal");
+	let error = null,
+		contexturaGrantTipo,
+		contexturaGrantValor,
+		pesoIdealBrocca;
+	const customer = await Customer.findById({ _id });
+	//console.log(customer, 'sal');
 
-  const pesoIdeal = tallaCM - 100;
-  if (customer.sex === "M") {
-    console.log("Sex masculino");
-    // PESO IDEAL O TEÓRICO: 1. Hamwi
-    pesoIdealBrocca = (tallaCM - 152) * 1.08 + 48.5;
+	const pesoIdeal = tallaCM - 100;
+	if (customer.sex === 'M') {
+		console.log('Sex masculino');
+		// PESO IDEAL O TEÓRICO: 1. Hamwi
+		pesoIdealBrocca = (tallaCM - 152) * 1.08 + 48.5;
 
-    console.log(customer);
-    contexturaGrantValor = tallaCM / circunferenciaMunieca;
+		//console.log(customer);
+		contexturaGrantValor = tallaCM / circunferenciaMunieca;
 
-    if (contexturaGrantValor > 5.4) {
-      contexturaGrantTipo = "Contextura Pequeña";
-    } else if (contexturaGrantValor > 9.6 && contexturaGrantValor <= 10.4) {
-      contexturaGrantTipo = "Contextura Mediana";
-    } else if (contexturaGrantValor <= 9.6) {
-      contexturaGrantTipo = "Contextura Grande";
-    }
-  } else {
-    pesoIdealBrocca = (tallaCM - 152) * 1.08 + 48.5;
+		if (contexturaGrantValor > 5.4) {
+			contexturaGrantTipo = 'Contextura Pequeña';
+		} else if (contexturaGrantValor > 9.6 && contexturaGrantValor <= 10.4) {
+			contexturaGrantTipo = 'Contextura Mediana';
+		} else if (contexturaGrantValor <= 9.6) {
+			contexturaGrantTipo = 'Contextura Grande';
+		}
+	} else {
+		pesoIdealBrocca = (tallaCM - 152) * 1.08 + 48.5;
 
-    contexturaGrantValor = tallaCM / circunferenciaMunieca;
+		contexturaGrantValor = tallaCM / circunferenciaMunieca;
 
-    if (contexturaGrantValor > 6) {
-      contexturaGrantTipo = "Contextura Pequeña";
-    } else if (contexturaGrantValor > 10.4 && contexturaGrantValor <= 11) {
-      contexturaGrantTipo = "Contextura Mediana";
-    } else if (contexturaGrantValor <= 10.11) {
-      contexturaGrantTipo = "Contextura Grande";
-    }
-  }
-  //Actualizando los datos del cliente
-  Customer.updateOne(
-    { _id },
-    {
-      $set: {
-        pesoIdealBrocca: pesoIdealBrocca,
-        contexturaGrantValor: contexturaGrantValor,
-        contexturaGrantTipo: contexturaGrantTipo,
-      },
-    },
-    (err) => {
-      if (err) error = err;
-    }
-  );
-  if (!error) return true; else return false;
+		if (contexturaGrantValor > 6) {
+			contexturaGrantTipo = 'Contextura Pequeña';
+		} else if (contexturaGrantValor > 10.4 && contexturaGrantValor <= 11) {
+			contexturaGrantTipo = 'Contextura Mediana';
+		} else if (contexturaGrantValor <= 10.11) {
+			contexturaGrantTipo = 'Contextura Grande';
+		}
+	}
+	//Actualizando los datos del cliente
+	Customer.updateOne(
+		{ _id },
+		{
+			$set: {
+				pesoIdealBrocca: pesoIdealBrocca,
+				contexturaGrantValor: contexturaGrantValor,
+				contexturaGrantTipo: contexturaGrantTipo,
+			},
+		},
+		(err) => {
+			if (err) error = err;
+		}
+	);
+	if (!error) return true;
+	else return false;
 };
 const pesoIdealClinicaMayoWest = async (_id, tallaCM) => {
-  let error, pesoIdealClinicaMayoWestMT;
-  try {
-    console.log("Clinica")
-    const customer = await Customer.findById({ _id });
-    console.log(customer, "customer")
-    console.log(typeof tallaCM, "tipos")
-    if (typeof tallaCM === "number" && tallaCM >= 0) {
-      if (customer.sex === "M") {
-        pesoIdealClinicaMayoWestMT = 22.1 * (tallaCM / 100) * 2;
-      } else {
-        pesoIdealClinicaMayoWestMT = 20.6 * (tallaCM / 100) * 2;
-      }
-      console.log("asignado")
-      console.log(pesoIdealClinicaMayoWestMT, "clinica metros")
-      Customer.updateOne(
-        { _id },
-        {
-          $set: {
-            "pesoIdealClinicaMayoWestMetros": pesoIdealClinicaMayoWestMT
-          }
-        },
-        (err) => {
-          if (err) error = err;
-        }
-      );
-      return true;
-    } else {
-      return false
-    }
-  } catch (err) {
-    return false;
-  }
+	let error, pesoIdealClinicaMayoWestMT;
+	try {
+		console.log('Clinica');
+		const customer = await Customer.findById({ _id });
+		//console.log(customer, 'customer');
+		//console.log(typeof tallaCM, 'tipos');
+		if (typeof tallaCM === 'number' && tallaCM >= 0) {
+			if (customer.sex === 'M') {
+				pesoIdealClinicaMayoWestMT = 22.1 * (tallaCM / 100) * 2;
+			} else {
+				pesoIdealClinicaMayoWestMT = 20.6 * (tallaCM / 100) * 2;
+			}
+			console.log('asignado');
+			console.log(pesoIdealClinicaMayoWestMT, 'clinica metros');
+			Customer.updateOne(
+				{ _id },
+				{
+					$set: {
+						pesoIdealClinicaMayoWestMetros: pesoIdealClinicaMayoWestMT,
+					},
+				},
+				(err) => {
+					if (err) error = err;
+				}
+			);
+			return true;
+		} else {
+			return false;
+		}
+	} catch (err) {
+		return false;
+	}
 };
 const pesoIdealIMC = async (req, res, next) => {
-  try {
-    const { tallaCM, circunferenciaMunieca, idCustomer } = req.body;
-    const customer = await Customer.findById({ _id: idCustomer });
+	try {
+		const { tallaCM, circunferenciaMunieca, idCustomer } = req.body;
+		const customer = await Customer.findById({ _id: idCustomer });
 
-    if (customer.sex == "M") {
-      const pesoIdealClinicaMayoWestMT = 22.1 * (tallaCM / 100) * 2;
-    } else {
-      const pesoIdealClinicaMayoWestMT = 20.6 * (tallaCM / 100) * 2;
-    }
-    next();
-  } catch (error) { }
+		if (customer.sex == 'M') {
+			const pesoIdealClinicaMayoWestMT = 22.1 * (tallaCM / 100) * 2;
+		} else {
+			const pesoIdealClinicaMayoWestMT = 20.6 * (tallaCM / 100) * 2;
+		}
+		next();
+	} catch (error) {}
 };
 module.exports = {
-  elegirPesoIdeal,
+	pesoIdealHamwi,
+	pesoIdealBrocca,
+	pesoIdealIMC,
+	pesoIdealClinicaMayoWest,
 };
